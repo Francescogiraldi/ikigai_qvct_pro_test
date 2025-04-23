7e1ddddbb53ddc6a08d30eb73435120b65195808
mystofj <jo.joyeux@gmail.com>
2025-04-23 13:11:13 +0200

    first commit

--- Duplicate-IKIGAIAPP-04-04-2025-master/src/backend/api.js
+++
@@ -1,8 +0,0 @@
-/**
- * Point d'entrée principal de l'API backend
- * Ce fichier exporte l'API centralisée depuis le dossier api/index.js
- */
-
-import API from './api/index';
-
-export default API;
